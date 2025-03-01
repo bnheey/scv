7a7347793bafdeaa89e639b8c905b407e68c7dd7
HEEYEON <bbjoite09@naver.com>
2025-03-01 18:48:00 +0900

    Feat: 버튼 컴포넌트화

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,54 @@
+import clsx from "clsx";
+import { ReactNode } from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  color?: "text" | "pink" | "default";
+  size?: "sm" | "md" | "lg";
+  className?: string;
+}
+
+const Button = ({
+  children,
+  color = "pink",
+  size = "md",
+  className,
+  ...props
+}: ButtonProps) => {
+  const getClassName = () => {
+    switch (color) {
+      case "text":
+        return "bg-transparent text-scv-pink border border-scv-pink";
+      case "pink":
+      case "default":
+        return "bg-scv-pink text-white";
+    }
+  };
+
+  const getSize = () => {
+    switch (size) {
+      case "sm":
+        return "px-2 py-1";
+      case "md":
+        return "px-4 py-2";
+      case "lg":
+        return "px-6 py-[10px]";
+    }
+  };
+
+  return (
+    <button
+      className={clsx(
+        `bg-scv-pink rounded-lg`,
+        getClassName(),
+        getSize(),
+        className
+      )}
+      {...props}
+    >
+      <p className="!font-pretendard-medium text-[14px]">{children}</p>
+    </button>
+  );
+};
+
+export default Button;
