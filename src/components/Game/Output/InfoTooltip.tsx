e103f841dea2eb2275ce076188ff8f87d8b9e6e8
bnheey <bbjoite09@naver.com>
2025-03-07 13:48:43 +0900

    Feat: [경기표 생성] 게스트 text 컬러 변경, info tooltip에 신입, 게스트 관련 정보 추가

--- src/components/Game/Output/InfoTooltip.tsx
+++ src/components/Game/Output/InfoTooltip.tsx
@@ -38,30 +38,54 @@ const InfoTooltip = () => {
           ref={tooltipRef}
           className="py-3 px-4 absolute right-0 w-[300px] bg-white border-[2px] rounded-md border-scv-pink"
         >
-          <div className="flex items-center justify-between mb-3">
+          <div className="flex items-center justify-between mb-4">
             <Text type="subTitleBlack">부가 설명</Text>
             <button className="p-0" onClick={() => setIsOpen(false)}>
               <X />
             </button>
           </div>
           <div className="flex flex-col gap-2">
-            <div className="flex items-center gap-1">
-              <div className="w-[60px] h-[30px] border border-red-600 rounded-md leading-[30px]">
-                방희연
+            <Text className="text-left mb-2" type="normalMediumBlack">
+              Player 정보
+            </Text>
+            <div className="flex flex-col gap-1 ml-1">
+              <div className="flex items-center gap-1">
+                <div className="px-1 py-1 border border-gray-200 rounded-md">
+                  희연🐣
+                </div>
+                <Text>: 신입</Text>
               </div>
-              <Text>: 밸런스 붕괴 ⛔️</Text>
+              <div className="flex items-center gap-1">
+                <div className="px-1 py-1 border border-gray-200 text-gray-400 rounded-md">
+                  방희연
+                </div>
+                <Text>: 게스트</Text>
+              </div>
             </div>
-            <div className="flex items-center gap-1">
-              <div className="w-[60px] h-[30px] border border-orange-300 rounded-md leading-[30px]">
-                방희연
+            {/* ---------------------------------------------------- */}
+            <hr className=" text-gray-200 my-2 border-[-1px]" />
+            <Text className="text-left mb-2" type="normalMediumBlack">
+              Game 정보
+            </Text>
+            <div className="flex flex-col gap-1 ml-1">
+              <div className="flex items-center gap-1">
+                <div className="px-1 py-1 border border-red-600 rounded-md">
+                  방희연
+                </div>
+                <Text>: 밸런스 붕괴 ⛔️</Text>
               </div>
-              <Text>: 양 팀 티어가 약간 차이나요 ⚠️</Text>
-            </div>
-            <div className="flex items-center gap-1">
-              <div className="text-white w-[60px] h-[30px] bg-red-600 rounded-md leading-[30px]">
-                방희연
+              <div className="flex items-center gap-1">
+                <div className="px-1 py-1 border border-orange-300 rounded-md">
+                  방희연
+                </div>
+                <Text>: 양 팀 티어가 약간 차이나요 ⚠️</Text>
               </div>
-              <Text>: 게임 내 중복 멤버가 있어요 ❌</Text>
+              <div className="flex items-center gap-1">
+                <div className="px-1 py-1 text-white bg-red-600 border border-red-600 rounded-md">
+                  방희연
+                </div>
+                <Text>: 게임 내 중복 멤버가 있어요 ❌</Text>
+              </div>
             </div>
           </div>
         </div>
